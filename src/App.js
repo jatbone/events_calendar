8df7cea3d4d110fcd31a328ff688ba91890a1f9a
Jakub <jakub.pivonka@gmail.com>
2019-05-25 15:21:03 +0200

    Base styles Materialui Materialui theme setup Materialui CssBaseline

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import moment from 'config/moment';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import reducer from 'reducer';
 import { StateProvider } from 'context/State';
+
+import 'typeface-roboto';
+
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+    fontFamily: [
+      'Roboto',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"'
+    ].join(',')
+  },
+  palette: {
+    background: {
+      default: '#fff'
+    }
+  }
+});
 
 function App() {
   const initialState = {
@@ -14,7 +41,10 @@ function App() {
   };
   return (
     <StateProvider initialState={initialState} reducer={reducer}>
-      App
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        App
+      </MuiThemeProvider>
     </StateProvider>
   );
 }
