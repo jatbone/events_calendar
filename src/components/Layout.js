041f99d07e0260169b6979caafcd005db7a108cc
Jakub <jakub.pivonka@gmail.com>
2019-05-26 17:44:19 +0200

    Layout styling

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,10 +3,18 @@ import React from 'react';
 import Calendar from 'components/Calendar/index';
 import Grid from '@material-ui/core/Grid';
 import Sidebar from 'components/Sidebar';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  }
+}));
 
 const Layout = () => {
+  const classes = useStyles();
   return (
-    <Grid container>
+    <Grid container className={classes.root}>
       <Calendar />
       <Sidebar />
     </Grid>
